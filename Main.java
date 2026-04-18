54f777ab9aeb0fde69dd78aefa4272573c4ccfcc
JOHNNY PHAN <phangia223@gmail.com>
2026-04-18 11:16:19 +0700

    Phuong trinh bat 2

--- Main.java
+++ Main.java
@@ -1,10 +1,21 @@
 package Quanly;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Thong tin ne thay");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap ten: ");
+		String a=sc.nextLine();
+		System.out.println(" NHap vao nam sinh cua ban: ");
+		int ns=sc.nextInt();
+		int kq=2026-ns;
+		
+		sc.close();
+		
+		System.out.println(a);
+		System.out.println(" Tuoi cua ban la : "+ kq);
 
 	}
 	
